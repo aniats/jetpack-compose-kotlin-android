a087e2bf894a0b6748b1c3decf0ee64b508e268d
Anna Tselikova <anna.tselikova@mdinc.ru>
2021-07-05 15:07:34 +0300

    My first commit

---
+++ app/src/main/java/com/raywenderlich/android/composableCookBook/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.raywenderlich.android.composableCookBook
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.TextStyle
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    // setContentView(R.layout.activity_main)
+
+    setContent() {
+      Column(modifier = Modifier.fillMaxSize()) {
+        // 2
+        TopAppBar(title = {
+          Text("ComposableCookBook")
+        })
+        // 3
+        RecipeList(defaultRecipes)
+      }
+    }
+  }
+
+  @Composable
+  @Preview
+  fun Greeting() {
+    Column() {
+      Text("Hello, World!", style = TextStyle(color = Color.Red))
+      Text("Hello, Second World!", style = TextStyle(color = Color.Red))
+      Text("Hello, Third World!", style = TextStyle(color = Color.Red))
+    }
+  }
+}
+
